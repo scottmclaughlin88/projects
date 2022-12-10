359c402bb3cb5536009d1e1152b29e2b3e638163
scottmclaughlin88 <110846892+scottmclaughlin88@users.noreply.github.com>
2022-12-10 17:54:05 -0600

    New additions to projects.

--- AI Algorithm/lite-ai.py
+++ AI Algorithm/lite-ai.py
@@ -1,3 +1,7 @@
+#This code will Analyze tweets and store them in a dictionary
+#then score them based on the top 10 worst and best tweets based on word scoring
+#A .csv file is automatically created and formatted with this information.
+
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
--- README.md
+++ README.md
@@ -1,3 +1,1 @@
-#This code will Analyze tweets and store them in a dictionary
-#then score them based on the top 10 worst and best tweets based on word scoring
-#A .csv file is automatically created and formatted with this information.
+#My GitHub projects.
